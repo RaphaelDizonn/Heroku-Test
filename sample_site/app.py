5569dac3dbb26be194007004509dc83abdea7cf4
RaphaelDizonn <90470101+RaphaelDizonn@users.noreply.github.com>
2022-03-28 12:16:26 -0400

    Update app.py

--- sample_site/app.py
+++ sample_site/app.py
@@ -11,7 +11,7 @@ socketio = SocketIO(app, cors_allowed_origins='*')
 
 @app.route('/') 
 def home():
- return render_template('index.html')
+ return render_template('http://lab3testttt.herokuapp.com/')
 
 @socketio.on('message')
 def handleMessage(msg):

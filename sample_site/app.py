68f75ff4465e489dd780b0399337e7b85220cb93
RaphaelDizonn <90470101+RaphaelDizonn@users.noreply.github.com>
2022-03-28 01:58:55 -0400

    asd

--- sample_site/app.py
+++ sample_site/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecret'
 
 
-socketio = SocketIO(app, cors_allowed_origins='*')
+socketio = SocketIO(app, cors_allowed_origins='*',  engineio_logger=True, logger=True)
 
 @app.route('/') 
 def home():

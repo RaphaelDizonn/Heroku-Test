f620db342e780763ba2e6b0e9d95ae4a83eaa5f7
RaphaelDizonn <90470101+RaphaelDizonn@users.noreply.github.com>
2022-03-28 02:07:44 -0400

    tes

--- sample_site/app.py
+++ sample_site/app.py
@@ -1,20 +1,26 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO, send, emit
-
-app = Flask(__name__, template_folder=".")
-app.config["SECRET_KEY"] = "fake key man"
-socketio = SocketIO(app, cors_allowed_origins="*", engineio_logger=True, logger=True)
+from flask_socketio import SocketIO, send
 
 
-@app.route("/")
-def index():
-    return render_template("index.html")
 
-    
-@socketio.on("message")
-def on_message(message):
-    print(f"message: {message}")
-    send("Hey, from the server")
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecret'
 
-if __name__ == "__main__":
-    socketio.run(app)
+
+socketio = SocketIO(app, cors_allowed_origins='*')
+
+@app.route('/') 
+def home():
+ return render_template('index.html')
+
+@socketio.on('message')
+def handleMessage(msg):
+	print('Message: ' + msg)
+	send(msg, broadcast=True)
+
+
+
+
+if __name__ == '__main__':
+	socketio.run(app)
+

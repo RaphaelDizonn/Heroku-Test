98ec5f6d940b0dd07e4c25d0fe1184d727db2231
RaphaelDizonn <90470101+RaphaelDizonn@users.noreply.github.com>
2022-03-28 03:22:46 -0400

    test

--- sample_site/app.py
+++ sample_site/app.py
@@ -1,26 +1,25 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO, send
-
-
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '39ccdde2930edbf62e9efcb5bc318da5dc8b58d9978de52c3d43cda83fbd3a49'
+app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
+socketio = SocketIO(app)
 
 
-socketio = SocketIO(app, cors_allowed_origins='*')
-
-@app.route('/') 
-def home():
- return render_template('index.html')
-
-@socketio.on('message')
-def handleMessage(msg):
-	print('Message: ' + msg)
-	send(msg, broadcast=True)
+@app.route('/')
+def sessions():
+    return render_template('index.html')
 
 
+def messageReceived(methods=['GET', 'POST']):
+    print('message was received!!!')
+
+
+@socketio.on('my event')
+def handle_my_custom_event(json, methods=['GET', 'POST']):
+    print('received my event: ' + str(json))
+    socketio.emit('my response', json, callback=messageReceived)
 
 
 if __name__ == '__main__':
-	socketio.run(app)
-
+    socketio.run(app, debug=True)

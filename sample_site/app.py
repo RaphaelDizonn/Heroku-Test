ee7b6e5cb4c38f20a55ce22e7818f6bc0bb6db67
RaphaelDizonn <90470101+RaphaelDizonn@users.noreply.github.com>
2022-03-27 23:35:43 -0400

    Update app.py

--- sample_site/app.py
+++ sample_site/app.py
@@ -5,13 +5,13 @@ from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecret'
+
+
 socketio = SocketIO(app, cors_allowed_origins='*')
 
-@app.route('/')
-
-
-def index():
-	return render_template('client.html')
+@app.route('/') 
+def home():
+ return render_template('index.html')
 
 @socketio.on('message')
 def handleMessage(msg):
@@ -23,3 +23,4 @@ def handleMessage(msg):
 
 if __name__ == '__main__':
 	socketio.run(app)
+

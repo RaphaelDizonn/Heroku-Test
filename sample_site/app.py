26dff81aa1bebb58822c463c97a34f62c67409f8
RaphaelDizonn <90470101+RaphaelDizonn@users.noreply.github.com>
2022-03-28 01:35:59 -0400

    ea

--- sample_site/app.py
+++ sample_site/app.py
@@ -1,26 +1,17 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO, send
-
-
+from flask import Flask, render_template, make_response, redirect
+from flask_socketio import SocketIO, send, emit
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'mysecret'
+socketio = SocketIO(app)
 
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-socketio = SocketIO(app, cors_allowed_origins='*')
-
-@app.route('/') 
-def home():
- return render_template('index.html')
-
-@socketio.on('message')
-def handleMessage(msg):
-	print('Message: ' + msg)
-	send(msg, broadcast=True)
-
-
-
-
-if __name__ == '__main__':
-	socketio.run(app)
-
+@socketio.on("message")
+def handleMessage(data):
+    emit("new_message",data,broadcast=True)
+    
+if __name__ == "__main__":
+    socketio.run(app, debug=True, host='0.0.0.0', port=5004)
